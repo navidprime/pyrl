c179cf11ee3221eb79c8727249d137328711cc4f
Navid Poladi <scific.navid@gmail.com>
2024-03-27 04:24:13 +0330

    add new features (get_qsize, no mapping for actions)

--- agent.py
+++ agent.py
@@ -1,9 +1,11 @@
 import numpy as np
+import sys
 import pickle
 
 class QLearning:
-    def __init__(self, n_actions:int, lr:float=0.1, gamma:float=.99, epsilon_length:int=200) -> None:
-        self.n_actions = n_actions
+    def __init__(self, actions:list, lr:float=0.1, gamma:float=.99, epsilon_length:int=200) -> None:
+        self.actions = actions
+        self.n_actions = len(actions)
         self.lr = lr
         self.gamma = gamma
         self.epsilon_length = epsilon_length
@@ -16,25 +18,34 @@ class QLearning:
     def __get_random_array(self):
         return np.random.normal(loc=0, scale=.1, size=(self.n_actions,)).astype('float32')
     
+    def get_qsize(self):
+        """get (1) number of elements in q_values, (2) size of (estimated) space that q_values use(in bytes)"""
+        total_size = sys.getsizeof(self.__q_values)  # Size of the dictionary itself
+        for value in self.__q_values.values():
+            total_size += sys.getsizeof(value)  # Size of each NumPy array
+            total_size += value.nbytes  # Size of the actual NumPy array data
+        return (len(self.__q_values),total_size)
+    
     def predict(self, state):
         random_choice_proba = np.random.randint(0, self.epsilon_length)
         if (self.__epsilon < random_choice_proba) and (not self.__test_mode):
             self.__epsilon += 1
-            return np.random.randint(0, self.n_actions)
+            return self.actions[np.random.randint(0, self.n_actions)]
         else:
             state = tuple(state)
             
             if state in self.__q_values.keys():
-                return np.argmax(self.__q_values[state])
+                return self.actions[np.argmax(self.__q_values[state])]
             else:
                 self.__q_values[state] = self.__get_random_array()
 
-                return np.random.randint(0, self.n_actions)
+                return self.actions[np.random.randint(0, self.n_actions)]
     
     def learn(self, state, action, new_state, reward):
         state = tuple(state)
         new_state = tuple(new_state)
-        
+        action = self.actions.index(action)
+
         if not state in self.__q_values.keys():
             self.__q_values[state] = self.__get_random_array()
             
--- example_usage.py
+++ example_usage.py
@@ -2,32 +2,22 @@ from game import *
 from agent import QLearning
 import msvcrt,os,time
 
-def mapping(x:int):
-    if (x == 0):
-        return Move.Left
-    elif (x == 1):
-        return Move.Right
-    elif (x == 2):
-        return Move.Up
-    elif (x == 3):
-        return Move.Down
-
+actions = [Move.Left, Move.Right, Move.Up, Move.Down]
 total = 100_000
-agent = QLearning(4,epsilon_length=int(total/2))
+agent = QLearning(actions,epsilon_length=int(total/2))
 game = Game()
 
 for i in range(total):
-    action_i = agent.predict(game.pos)
-    action = mapping(action_i)
+    action = agent.predict(game.pos)
     prev_pos, cur_pos, status = game.move(action)
     reward = 10 if status == Status.Win else -10 if status == Status.Lose else 0
 
-    agent.learn(prev_pos, action_i, cur_pos, reward)
+    agent.learn(prev_pos, action, cur_pos, reward)
 
 agent.save_q("saved.pkl")
+# print(agent.get_qsize())
 
-
-agent2 = QLearning(4)
+agent2 = QLearning([Move.Left, Move.Right, Move.Up, Move.Down])
 agent2.load_q("saved.pkl")
 agent2.exit_train_mode()
 while True:
@@ -35,7 +25,7 @@ while True:
     game.show_map()
     time.sleep(1)
 
-    move = mapping(agent2.predict(game.pos))
+    move = agent2.predict(game.pos)
 
     prev_pos, cur_pos, status = game.move(move)
     if (status == Status.Lose):
